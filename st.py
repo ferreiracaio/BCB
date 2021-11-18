b37bcf6af649cd404aecaf4e2a2b16d5d8977aaa
ferreiracaio <86454432+ferreiracaio@users.noreply.github.com>
2021-11-17 23:00:55 -0300

    Update st.py

--- st.py
+++ st.py
@@ -2,12 +2,9 @@
 import pandas as pd 
 import numpy as np
 import datetime as dt
-import sklearn
-import pandas_datareader.data as web
-import statsmodels.api as sm
-import random
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn import metrics
-from sklearn.metrics import mean_squared_error
+opp = pd.read_json("http://api.bcb.gov.br/dados/serie/bcdata.sgs.433/dados?formato=json")
+
+import streamlit as st
+
+st.write("Hello")
 
