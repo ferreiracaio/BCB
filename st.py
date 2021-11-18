15320f04e7c73f9af80270fbc18accc5d52e4e06
ferreiracaio <86454432+ferreiracaio@users.noreply.github.com>
2021-11-17 23:12:40 -0300

    Update st.py

--- st.py
+++ st.py
@@ -14,9 +14,12 @@ x = st.slider('x')  # 👈 this is a widget
 
 st.write(x, 'Mova o cursor para alterar o índice', opp.iloc[0:x,:])
 
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
 
-
-
+    chart_data
 
 
 
