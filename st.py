58d629fc660a88aff8d698383eb3931aa442e165
ferreiracaio <86454432+ferreiracaio@users.noreply.github.com>
2021-11-17 23:10:23 -0300

    Update st.py

--- st.py
+++ st.py
@@ -2,9 +2,23 @@
 import pandas as pd 
 import numpy as np
 import datetime as dt
-opp = pd.read_json("http://api.bcb.gov.br/dados/serie/bcdata.sgs.433/dados?formato=json")
-
 import streamlit as st
 
-st.write("Hello")
+
+opp = pd.read_json("http://api.bcb.gov.br/dados/serie/bcdata.sgs.433/dados?formato=json")
+opp['valor'] = opp['valor'].replace(r'^\s*$', np.nan, regex=True).astype(float)
+opp['data'] = pd.to_datetime(opp['data'], format = '%d/%m/%Y')
+opp.columns = ['data','ipca']
+
+x = st.slider('x')  # 👈 this is a widget
+
+st.write(x, 'Mova o cursor para alterar o índice', opp.iloc[0:x,:])
+
+
+
+
+
+
+
+
 
