f9a1fe8f97861074440e8516f88996159d7988e1
ferreiracaio <86454432+ferreiracaio@users.noreply.github.com>
2021-11-17 22:58:08 -0300

    Update st.py

--- st.py
+++ st.py
@@ -1,1 +1,13 @@
+import matplotlib.pyplot as plt
+import pandas as pd 
+import numpy as np
+import datetime as dt
+import sklearn
+import pandas_datareader.data as web
+import statsmodels.api as sm
+import random
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import metrics
+from sklearn.metrics import mean_squared_error
 
