a4ebcddd26a9caee09cb2d9b32985e7d7c9935c2
ferreiracaio <86454432+ferreiracaio@users.noreply.github.com>
2021-11-17 23:35:52 -0300

    Update st.py

--- st.py
+++ st.py
@@ -11,7 +11,8 @@ opp.columns = ['data','ipca']
 
 x = st.slider('x')  # 👈 this is a widget
 
-st.write(x, 'Mova o cursor para alterar o índice', opp.iloc[0:x,:],opp.iloc[0:x,:].plot())
+st.write(x, 'Mova o cursor para alterar o índice', opp.iloc[0:x,:])
+st.write(opp.iloc[0:x,:].plot())
 
 
 
